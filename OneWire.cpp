2de343d6b91b62cc1a31cd589d1e455ed9db6c0c
dobby84 <95171123+dobby84@users.noreply.github.com>
2021-12-02 17:08:24 +0100

    Add files via upload

--- OneWire.cpp
+++ OneWire.cpp
@@ -7,139 +7,147 @@
 
 //#define OVERDRIVE
 
-#ifndef OVERDRIVE			// Time definition from Maxim application node
-	#define _timeA 6		// Write: time for "1" bit
-	#define _timeB 54		// Write: time keep bus high after writing "1" bit (64us - 10us = 54us)
-	#define _timeC 54		// Write: time for "0" bit (60us - 6us = 54us)
-	#define _timeD 10		// Write: time keep bus high after writing "0" bit
-	#define _timeE 9		// Read: time until sample input state
-	#define _timeF 55		// Read: time for recovery after reading
-	#define _timeH 480		// Reset: time for drive bus low
-	#define _timeI 70		// Reset: time until sample input state
-	#define _timeG 0		// Reset: delay before reset pulse
-	#define _timeJ 410		// Reset: time for become bus high after present pulse
+#ifndef OVERDRIVE	    // Time definition from Maxim application node
+    #define _timeA 6    // Write: time for "1" bit
+    #define _timeB 54   // Write: time keep bus high after writing "1" bit (64us - 10us = 54us)
+    #define _timeC 54   // Write: time for "0" bit (60us - 6us = 54us)
+    #define _timeD 10   // Write: time keep bus high after writing "0" bit
+    #define _timeE 9    // Read: time until sample input state
+    #define _timeF 55   // Read: time for recovery after reading
+    #define _timeH 480  // Reset: time for drive bus low
+    #define _timeI 70   // Reset: time until sample input state
+    #define _timeG 0    // Reset: delay before reset pulse
+    #define _timeJ 410  // Reset: time for become bus high after present pulse
 #else
-	#define	_timeH 70		// Reset: time for drive bus low
-	#define	_timeI 8.5 		// Reset: time for release bus
-	#define	_timeG 2.5		// Reset: delay before reset pulse
+    #define	_timeH 70   // Reset: time for drive bus low
+    #define	_timeI 8.5  // Reset: time for release bus
+    #define	_timeG 2.5  // Reset: delay before reset pulse
 #endif
 
-#define MATCH_ROM 0x55
-#define SKIP_ROM 0xCC
-#define READ_SCRATCH 0xBE
-#define CONVERT_T 0x44
-#define CONVERT_V 0xB4
-#define WRITE_SCRATCH 0x4E
-#define RECALL_MEMORY 0xB8
-#define COPY_SCRATCH 0x48
-
+#define MATCH_ROM       0x55
+#define SKIP_ROM        0xCC
+#define READ_SCRATCH    0xBE
+#define CONVERT_T       0x44
+#define CONVERT_V       0xB4
+#define WRITE_SCRATCH   0x4E
+#define RECALL_MEMORY   0xB8
+#define COPY_SCRATCH    0x48
 
 #define F_CPU 16000000UL
 #include <util/delay.h>
 #include "OneWire.h"
 
 static uint8_t crcTable[] = {
-	0x00, 0x5E, 0xBC, 0xE2, 0x61, 0x3F, 0xDD, 0x83, 0xC2, 0x9C, 0x7E, 0x20, 0xA3, 0xFD, 0x1F, 0x41,
-	0x9D, 0xC3, 0x21, 0x7F, 0xFC, 0xA2, 0x40, 0x1E, 0x5F, 0x01, 0xE3, 0xBD, 0x3E, 0x60, 0x82, 0xDC,
-	0x23, 0x7D, 0x9F, 0xC1, 0x42, 0x1C, 0xFE, 0xA0, 0xE1, 0xBF, 0x5D, 0x03, 0x80, 0xDE, 0x3C, 0x62,
-	190,224,  2, 92,223,129, 99, 61,124, 34,192,158, 29, 67,161,255,
-	70, 24,250,164, 39,121,155,197,132,218, 56,102,229,187, 89,  7,
-	219,133,103, 57,186,228,  6, 88, 25, 71,165,251,120, 38,196,154,
-	101, 59,217,135,  4, 90,184,230,167,249, 27, 69,198,152,122, 36,
-	248,166, 68, 26,153,199, 37,123, 58,100,134,216, 91,  5,231,185,
-	140,210, 48,110,237,179, 81, 15, 78, 16,242,172, 47,113,147,205,
-	17, 79,173,243,112, 46,204,146,211,141,111, 49,178,236, 14, 80,
-	175,241, 19, 77,206,144,114, 44,109, 51,209,143, 12, 82,176,238,
-	50,108,142,208, 83, 13,239,177,240,174, 76, 18,145,207, 45,115,
-	202,148,118, 40,171,245, 23, 73,  8, 86,180,234,105, 55,213,139,
-	87,  9,235,181, 54,104,138,212,149,203, 41,119,244,170, 72, 22,
-	233,183, 85, 11,136,214, 52,106, 43,117,151,201, 74, 20,246,168,
-116, 42,200,150, 21, 75,169,247,182,232, 10, 84,215,137,107, 53};
+    0x00, 0x5E, 0xBC, 0xE2, 0x61, 0x3F, 0xDD, 0x83, 0xC2, 0x9C, 0x7E, 0x20, 0xA3, 0xFD, 0x1F, 0x41,
+    0x9D, 0xC3, 0x21, 0x7F, 0xFC, 0xA2, 0x40, 0x1E, 0x5F, 0x01, 0xE3, 0xBD, 0x3E, 0x60, 0x82, 0xDC,
+    0x23, 0x7D, 0x9F, 0xC1, 0x42, 0x1C, 0xFE, 0xA0, 0xE1, 0xBF, 0x5D, 0x03, 0x80, 0xDE, 0x3C, 0x62,
+    190,224,  2, 92,223,129, 99, 61,124, 34,192,158, 29, 67,161,255,
+    70, 24,250,164, 39,121,155,197,132,218, 56,102,229,187, 89,  7,
+    219,133,103, 57,186,228,  6, 88, 25, 71,165,251,120, 38,196,154,
+    101, 59,217,135,  4, 90,184,230,167,249, 27, 69,198,152,122, 36,
+    248,166, 68, 26,153,199, 37,123, 58,100,134,216, 91,  5,231,185,
+    140,210, 48,110,237,179, 81, 15, 78, 16,242,172, 47,113,147,205,
+    17, 79,173,243,112, 46,204,146,211,141,111, 49,178,236, 14, 80,
+    175,241, 19, 77,206,144,114, 44,109, 51,209,143, 12, 82,176,238,
+    50,108,142,208, 83, 13,239,177,240,174, 76, 18,145,207, 45,115,
+    202,148,118, 40,171,245, 23, 73,  8, 86,180,234,105, 55,213,139,
+    87,  9,235,181, 54,104,138,212,149,203, 41,119,244,170, 72, 22,
+    233,183, 85, 11,136,214, 52,106, 43,117,151,201, 74, 20,246,168,
+    116, 42,200,150, 21, 75,169,247,182,232, 10, 84,215,137,107, 53
+};
 
-/*-------------------------------------------------------------------------------------------------
-* OneWire Interface
-*--------------------------------------------------------------------------------------------------
-*/
+//---------------------------------------------------------------------------------
+// Class OneWire Interface
+//---------------------------------------------------------------------------------
+//
 OwInterface::OwInterface(volatile uint8_t* ddir, volatile uint8_t* port, volatile uint8_t* pinr, const uint8_t pin){
-	ddir_ = (uint8_t*)ddir;
-	port_ = (uint8_t*)port;
-	pinr_ = (uint8_t*)pinr;
-	pin_ = (1 << pin);			// save configured pin number
-	*ddir_ &= (~pin_);			// config pin as input
-	*port_ &= (~pin_);			// set output register drive pin low
+    ddir_ = (uint8_t*)ddir;
+    port_ = (uint8_t*)port;
+    pinr_ = (uint8_t*)pinr;
+    pin_ = (1 << pin);      // save configured pin number
+    *ddir_ &= (~pin_);      // config pin as input
+    *port_ &= (~pin_);      // set output register drive pin low
 }
 
 bool OwInterface::reset(){
-	bool resetDone = 0;
-	_delay_us(_timeG);
-	*ddir_ |= pin_;
-	_delay_us(_timeH);
-	*ddir_ &= (~pin_);
-	_delay_us(_timeI);
-	resetDone = !(*pinr_ & pin_);
-	_delay_us(_timeJ);
-	return resetDone;
+    bool resetDone = 0;
+    _delay_us(_timeG);
+    *ddir_ |= pin_;
+    _delay_us(_timeH);
+    *ddir_ &= (~pin_);
+    _delay_us(_timeI);
+    resetDone = !(*pinr_ & pin_);
+    _delay_us(_timeJ);
+    return resetDone;
 }
 
 bool OwInterface::readBit(){
-	bool value;
-	*ddir_ |= pin_;
-	_delay_us(_timeA);
-	*ddir_ &= (~pin_);
-	_delay_us(_timeE);
-	value = (*pinr_ & pin_);
-	_delay_us(_timeF);
-	return value;
+    bool value;
+    *ddir_ |= pin_;
+    _delay_us(_timeA);
+    *ddir_ &= (~pin_);
+    _delay_us(_timeE);
+    value = (*pinr_ & pin_);
+    _delay_us(_timeF);
+    return value;
 }
 
 void OwInterface::writeBit(int value){
-	*ddir_ |= pin_;
-	_delay_us(_timeA);
-	if (!value) _delay_us(_timeC);
-	*ddir_ &= (~pin_);
-	_delay_us(_timeD);
-	if (value) _delay_us(_timeB);
+    *ddir_ |= pin_;
+    _delay_us(_timeA);
+    if (!value) _delay_us(_timeC);
+    *ddir_ &= (~pin_);
+    _delay_us(_timeD);
+    if (value) _delay_us(_timeB);
 }
 
-/*-------------------------------------------------------------------------------------------------
-* OneWire Bus
-*--------------------------------------------------------------------------------------------------
-*/
+//---------------------------------------------------------------------------------
+// Class OneWire Bus
+//---------------------------------------------------------------------------------
+//
 OwBus::OwBus(OwInterface& interface){
-	interface_ = &interface;
+    interface_ = &interface;
 }
 
 bool OwBus::reset(){
-	return (*interface_).reset();
+    return (*interface_).reset();
 }
 
 void OwBus::sendByte(uint8_t bData){
-	int i;
-	for (i = 0; i < 8; i++){					// Loop to write each bit in the byte, LS-bit first
-		(*interface_).writeBit(bData & 0x01);	// shift the data byte for the next bit
-		bData >>= 1;
-	}
+    int i;
+    for (i = 0; i < 8; i++){                    // <-Loop to write every bit into
+        (*interface_).writeBit(bData & 0x01);   //   the byte, LSB first
+        bData >>= 1;                            // <-shifts the data bits for the
+    }                                           //   next bit
 }
 
 uint8_t OwBus::receiveByte(){
-	int i, result=0;
-	for (i = 0; i < 8; i++){
-		result >>= 1;							// shift the result to get it ready for the next bit
-		if ((*interface_).readBit())			// if result is one, then set MS bit
-		result |= 0x80;
-	}
-	return result;
+    int i, result=0;
+    for (i = 0; i < 8; i++){
+        result >>= 1;							// shift the result to get it ready for the next bit
+        if ((*interface_).readBit())			// if result is one, then set MS bit
+        result |= 0x80;
+    }
+    return result;
 }
 
-/*-------------------------------------------------------------------------------------------------
-* OneWire Device
-*--------------------------------------------------------------------------------------------------
-*/
+//---------------------------------------------------------------------------------
+// Class OneWire Device
+//---------------------------------------------------------------------------------
+//
 OwDevice::OwDevice(OwBus& bus){
-	bus_ = &bus;
-	interface_ = (*bus_).interface_;
+    bus_ = &bus;
+    interface_ = (*bus_).interface_;
 }
 
+//---------------------------------------------------------------------------------
+// Algorithm to searching present devices on the bus. 
+// 
+// return 1 - search successful and device found
+// return 0 - no or no more device found
+//
+// ToDo:
+//
 bool OwDevice::search(){
 	if (!(*interface_).reset() || lastDeviceFlag_){		// perform one wire reset
 		lastDiscrepancy_ = 0;
@@ -215,18 +223,18 @@ bool OwDevice::search(){
 }
 
 void OwDevice::matchRom(uint8_t* address){
-	uint8_t i = 0;
-	
-	(*bus_).sendByte(MATCH_ROM);
-	for (i = 0; i < 8; i++){					// Loop to write ROM address to bus
-		(*bus_).sendByte(*(address + i));
-	}
+    uint8_t i = 0;
+    
+    (*bus_).sendByte(MATCH_ROM);
+    for (i = 0; i < 8; i++){					// Loop to write ROM address to bus
+    	(*bus_).sendByte(*(address + i));
+    }
 }
 
 void OwDevice::resetSearch(){  // experimental
-	lastDiscrepancy_ = 0;
-	lastFamilyDiscrepancy_ = 0;
-	lastDeviceFlag_ = false;
+    lastDiscrepancy_ = 0;
+    lastFamilyDiscrepancy_ = 0;
+    lastDeviceFlag_ = false;
 }
 
 uint8_t OwDevice::calcCRC8(const uint8_t *data, uint8_t len){
@@ -257,9 +265,9 @@ uint8_t OwDevice::tableCRC8(uint8_t *p, uint8_t len)
 	}
 	return crc & 0xFF;
 }
-//----------------------------------------------------------------------------------------------------------------------
-// Writes data to the scratch pad and validates of correct transmission. Validation for correct transmission is
-// optional and disable by default.
+//---------------------------------------------------------------------------------
+// Writes data to the scratch pad and validates of correct transmission. Validation
+// for correct transmission is optional and disable by default.
 //
 // parameter 1  - ROM address of OneWire device
 // parameter 2  - byte to write in scratch pad
@@ -304,7 +312,7 @@ uint8_t OwDevice::writeScratch(uint8_t* address, uint8_t data, uint8_t page, uin
 	}
 	return 1;
 }
-//----------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 // Copies the scratch pad to target page in SRAM/EEPROM.
 //
 // parameter 1  - ROM address of OneWire device
@@ -326,6 +334,10 @@ uint8_t OwDevice::copyScratch(uint8_t* address, uint8_t page){
 	return 1;
 }
 
+//---------------------------------------------------------------------------------
+// Class OneWire DS18B20
+//---------------------------------------------------------------------------------
+//
 OwDS18B20::OwDS18B20(OwBus& bus) : OwDevice::OwDevice (bus){
 	bus_ = &bus;
 }
@@ -408,16 +420,19 @@ float OwDS18B20::getTempFloat(){					// calculate temperature in degrees Celsius
 	return (float)getTempRaw() / 16.0 - 55.0;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-// Constructor of class OwDS2438
+
+//---------------------------------------------------------------------------------
+// Class OneWire DS2438
+//---------------------------------------------------------------------------------
 //
-OwDS2438::OwDS2438(OwBus& bus) : OwDevice::OwDevice (bus){
+OwDS2438::OwDS2438(OwBus& bus) : OwDevice::OwDevice (bus){ // Constructor
 	bus_ = &bus;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-// Writes the configuration to the scratch pad, validates of correct transmission and copies the scratch pad to target
-// page in SRAM/EEPROM. Validation for correct transmission is optional and disable by default.
+//---------------------------------------------------------------------------------
+// Writes the configuration to the scratch pad, validates of correct transmission
+// and copies the scratch pad to target page in SRAM/EEPROM. Validation for correct
+// transmission is optional and disable by default.
 //
 // parameter 1  - ROM address of one wire device
 // parameter 2  - byte to write in register
@@ -440,7 +455,7 @@ uint8_t OwDS2438::setConfiguration(uint8_t* address, uint8_t config, uint8_t pag
 	*/
 }
 
-//----------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 // Sends temperature conversation command to bus.
 //
 // return true  - successful
@@ -458,8 +473,9 @@ bool OwDS2438::convertV(uint8_t* address){
 	return true;
 }
 
-//----------------------------------------------------------------------------------------------------------------------
-// Copies the source page from the SRAM/EEPROM to the target page in the scratchpad.
+//---------------------------------------------------------------------------------
+// Copies the source page from the SRAM/EEPROM to the target page in the
+// scratchpad.
 //
 // return true  - successful
 // return false - reset fault
--- OneWire.h
+++ OneWire.h
@@ -9,113 +9,121 @@
 
 #define F_CPU 16000000UL
 #include <util/delay.h>
-#include <util/delay_basic.h>
 
 class OwDevice;
 
-/*-------------------------------------------------------------------------------------------------
-* OneWire Interface
-*
-* Class provide low level communication on a physical interface. The class handles interface
-* configuration, reset routine and bit bang communication.
----------------------------------------------------------------------------------------------------
-*/
+//---------------------------------------------------------------------------------
+// OneWire Interface
+//
+// Class provide low level communication on a physical interface. The class handles
+// interface configuration, reset routine and bit bang communication.
+//---------------------------------------------------------------------------------
+//
 class OwInterface{
-	private:
-		uint8_t* ddir_;		// Data direction register
-		uint8_t* port_;		// Port out register
-		uint8_t* pinr_;		// Port in register
-		uint8_t  pin_;		// Pin of IO register
-	public:
-		bool reset();
-		void writeBit(int);
-		bool readBit();
-		
-		OwInterface(volatile uint8_t*, volatile uint8_t*,
-		            volatile uint8_t*, const uint8_t);		// Constructor
-		~OwInterface();										// Destructor
+    private:
+        uint8_t* ddir_;     // Data direction register
+        uint8_t* port_;     // Port out register
+        uint8_t* pinr_;     // Port in register
+        uint8_t  pin_;      // Pin of IO register
+    public:
+        bool reset();
+        void writeBit(int);
+        bool readBit();
+
+        OwInterface(volatile uint8_t*, volatile uint8_t*,   // Constructor
+                    volatile uint8_t*, const uint8_t);		
+        ~OwInterface();                                     // Destructor
 };
 
-/*-------------------------------------------------------------------------------------------------
-* OneWire Bus
-*
-* The class handles the byte communication on a specific interface. It provide byte send/receive
-* function on one wire bus.
----------------------------------------------------------------------------------------------------
-*/
+//---------------------------------------------------------------------------------
+// OneWire Bus
+// 
+// The class handles the byte communication on a specific interface. It provide
+// byte send/receive function on one wire bus.
+//
+// ToDo:
+// - count of present devices on bus
+// - managing present devices eg. ROM ID Array and access by index number
+//---------------------------------------------------------------------------------
+// 
 class OwBus{
-	private:
-		OwInterface* interface_;	// pointer to binded interface
-	public:
-		bool reset();
-		void sendByte(uint8_t);
-		uint8_t receiveByte();
-		OwBus(OwInterface&);
-		
-		friend class OwDevice;
+    private:
+        OwInterface* interface_;    // pointer to binded interface
+    public:
+        bool reset();
+        void sendByte(uint8_t);
+        uint8_t receiveByte();
+        OwBus(OwInterface&);
+
+        friend class OwDevice;
 };
 
-/*-------------------------------------------------------------------------------------------------
-* OneWire Device
-*
-* The Class handles the communication to a specific one wire device. The parent class provide
-* standard functions usable for any device on bus. The child classes provide special
-* functions for a specific device like DS18B20.
----------------------------------------------------------------------------------------------------
-*/
+//---------------------------------------------------------------------------------
+// OneWire Device
+// 
+// The Class handles the communication to a specific one wire device. The parent
+// class provide standard functions usable for any device on bus. The child classes
+// provide special functions for a specific device like DS18B20.
+//
+// ToDo:
+// -move search function into class "interface"
+//---------------------------------------------------------------------------------
+// 
 class OwDevice{
-	protected:
-		OwInterface* interface_;				// pointer to binded interface
-		OwBus* bus_;							// pointer to binded bus
-	private:
-		uint8_t lastDiscrepancy_ = 0;			//* variables for OneWire Search
-		uint8_t lastFamilyDiscrepancy_ = 0;
-		bool lastDeviceFlag_ = false;
-		uint8_t idBitNumber_ = 1;
-		uint8_t lastZero_ = 0;
-		bool idBit_ = false;
-		bool cmpIdBit_ = false;
-		bool searchDirection_ = false;
-		uint8_t bitMask_ = 1;
-		uint8_t byteMask_ = 0;
-		uint8_t romNo_[8];
-		uint8_t data_[9];
-	public:
-		bool search();
-		void matchRom(uint8_t*);
-		void resetSearch();
-		OwDevice(OwBus&);
-	protected:		
-		uint8_t calcCRC8(const uint8_t*, uint8_t);
-		uint8_t tableCRC8(uint8_t*, uint8_t);
-		uint8_t writeScratch(uint8_t*, uint8_t, uint8_t = 255, uint8_t = 0);
-		uint8_t copyScratch(uint8_t*, uint8_t);
+    public:
+        uint8_t numberOfDevices = 0;
+    protected:
+        OwInterface* interface_;                // pointer to binded interface
+        OwBus* bus_;                            // pointer to binded bus
+    private:
+        uint8_t lastDiscrepancy_ = 0;           // variables for OneWire Search
+        uint8_t lastFamilyDiscrepancy_ = 0;
+        bool lastDeviceFlag_ = false;
+        uint8_t idBitNumber_ = 1;
+        uint8_t lastZero_ = 0;
+        bool idBit_ = false;
+        bool cmpIdBit_ = false;
+        bool searchDirection_ = false;
+        uint8_t bitMask_ = 1;
+        uint8_t byteMask_ = 0;
+        uint8_t romNo_[8];
+        uint8_t data_[9];
+    public:
+        bool search();
+        void matchRom(uint8_t*);
+        void resetSearch();
+        OwDevice(OwBus&);
+    protected:		
+        uint8_t calcCRC8(const uint8_t*, uint8_t);
+        uint8_t tableCRC8(uint8_t*, uint8_t);
+        uint8_t writeScratch(uint8_t*, uint8_t, uint8_t = 255, uint8_t = 0);
+        uint8_t copyScratch(uint8_t*, uint8_t);
 };
 
 class OwDS18B20 : private OwDevice{
-	private:
-		uint8_t data_[9];
-	public:
-		bool setResolution(uint8_t* = 0, uint8_t = 12);
-		uint8_t getResolution(uint8_t* = 0);
-		bool convertT(uint8_t* = 0);
-		bool receiveTemp(uint8_t* = 0);
-		uint16_t getTempRaw();
-		float getTempFloat();
-		OwDS18B20(OwBus&);
+    private:
+        uint8_t data_[9];
+    public:
+        bool setResolution(uint8_t* = 0, uint8_t = 12);
+        uint8_t getResolution(uint8_t* = 0);
+        bool convertT(uint8_t* = 0);
+        bool receiveTemp(uint8_t* = 0);
+        uint16_t getTempRaw();
+        float getTempFloat();
+        OwDS18B20(OwBus&);
 };
 
 class OwDS2438 : private OwDevice{
-	public:
-		uint8_t data_[9];
-	public:
-		uint8_t setConfiguration(uint8_t*, uint8_t, uint8_t = 0, uint8_t = 0);
-		bool convertV(uint8_t* = 0);
-		bool recallMemory(uint8_t* = 0, uint8_t = 0x00);
-		bool receiveVolt(uint8_t* = 0, uint8_t = 0x00);
-		uint16_t getVoltRaw();
-		float getVoltFloat();
-		OwDS2438(OwBus&);
+    public:
+        uint8_t data_[9];
+    public:
+        uint8_t setConfiguration(uint8_t*, uint8_t, uint8_t = 0, uint8_t = 0);
+        bool convertV(uint8_t* = 0);
+        bool recallMemory(uint8_t* = 0, uint8_t = 0x00);
+        bool receiveVolt(uint8_t* = 0, uint8_t = 0x00);
+        uint16_t getVoltRaw();
+        float getVoltFloat();
+        OwDS2438(OwBus&);
 };
 
 #endif /* ONEWIRE_H_ */
